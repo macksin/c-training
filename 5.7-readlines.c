aeb9e4075005a49226513ec7f7b9d1176cf0cdd0
Mackson H. F. Mizumachi <mack.heyha@gmail.com>
2020-11-06 07:44:00 -0300

    NEW: Trying to modify how we writie to char pointer.

--- 5.7-readlines.c
+++ 5.7-readlines.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAXL 10000
+// Max contents of unique pointer
+#define MAXCONTENT 100000
 
-int getline1(char s[], int lim);
+int getline1(char *s, int lim, int start);
+void writelines1(char *lineptr[], int nlines);
 
 main()
 {
-	//	char teste[MAXL];
-	//	printf("==============\n");
-	//	while (getline1(teste, MAXL) > 0)
-	//		printf("\t%s\n", teste);
+		char *s[MAXL];
+		char line[MAXL];
+		getline1(line, MAXCONTENT, 0);
+		s[0] = line;
+		writelines1(s, 1);
+		getline1(line, MAXCONTENT, strlen(line));
+		s[1] = line;
+		writelines1(s, 2);
+		printf("TESTING\n");
+		printf("%s\n", s[0]);
+		printf("%s\n", s[1]);
 }
 
+/* Modified to write where we point to,
+ * so it can be used to store values
+ * not using alloc
+ * usage: getline1(storage, CHARLIMIT, where_to_start)
+ * */
 int
-readlines(char *s[], int maxlines)
-{
-	int len, line[MAXL];
-
-
-}
-
-int
-getline1(char s[], int lim)
+getline1(char *s, int lim, int start)
 {
 	int c, i;
+
+	s += start;
+
+	if (*s == '\0')
+		*s = ' ';
 
 	for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++)
 		*s++ = c;
@@ -34,4 +47,13 @@ getline1(char s[], int lim)
 	*s = '\0';
 
 	return i;
+}
+
+/* writelines:  write output lines */
+void writelines1(char *lineptr[], int nlines)
+{
+    int i;
+
+    for (i = 0; i < nlines; i++)
+        printf("%s\n", lineptr[i]);
 }
